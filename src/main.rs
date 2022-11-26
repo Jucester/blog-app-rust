90d253365da14e3567592b6ee5c2842a3cc451b1
Jucester <juceld@gmail.com>
2022-11-25 21:37:56 -0400

    advances in post

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ pub mod models;
 
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 use dotenv::dotenv;
-use models::{NewPost, Post};
+use models::{NewPost, Post, NewPostHandler};
 use std::env;
 
 use diesel::pg::PgConnection;
@@ -22,7 +22,6 @@ use self::schema::posts::dsl::*;
 pub type DbPool = r2d2::Pool<ConnectionManager<PgConnection>>;
 
 fn connect_db() -> DbPool {
-
     let db_url = env::var("DATABASE_URL").expect("Db url missing");
     // PgConnection::establish(&db_url).unwrap_or_else(|_| panic!("Connection failed to {}", db_url))
     //
@@ -34,10 +33,10 @@ fn insert(conn: &mut PgConnection, new_post: NewPost) -> Post {
     diesel::insert_into(posts::table).values(new_post).get_result::<Post>(conn).expect("Insert failed")
 }
 
-// #[get("/")]
-// async fn hello_world() -> impl Responder {
-//     HttpResponse::Ok().body("Hello World")
-// }
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello World")
+}
 
 
 #[get("/posts")]
@@ -46,26 +45,25 @@ async fn get_posts(pool: web::Data<DbPool>) -> impl Responder {
 
     match web::block(move || {posts.load::<Post>(&mut conn)}).await {
         Ok(data) => {
-            println!("{:?}", data);
+            HttpResponse::Ok().body(format!("{:?}", data))
+        },
+        Err(err) => HttpResponse::Ok().body("Fail")
+    }
+}
 
-            let res = String::new();
+#[post("/posts")]
+async fn create_post(pool: web::Data<DbPool>, item: web::Json<NewPostHandler>) -> impl Responder {
+    let mut conn = pool.get().expect("Error connecting db.");
 
-            // for post in data {
-            //     res = format!("{}\n{}\n", post.title, post.body);
-            // }
+    println!("{:?}", item);
 
-
+    match web::block(move || {Post::create(&mut conn, &item)}).await {
+        Ok(data) => {
             HttpResponse::Ok().body(format!("{:?}", data))
         },
         Err(err) => HttpResponse::Ok().body("Fail")
     }
 
-    // let posts_res = posts.load::<Post>(conn).expect("Error executing query");
-
-    // for post in posts_res {
-    //     println!("{:?}", post);
-    // }
-    //
 }
 
 
@@ -76,30 +74,15 @@ async fn main() -> std::io::Result<()> {
 
     let conn = connect_db();
     HttpServer::new(move || {
-        App::new().service(get_posts).app_data(web::Data::new(conn.clone()))
+        App::new()
+            .service(get_posts)
+            .service(create_post)
+            .app_data(web::Data::new(conn.clone()))
     }).bind(("0.0.0.0", 9900)).unwrap().run().await
-
-    // let conn = &mut connect_db();
-
-    // let mut post_title = String::new();
-    // let mut post_body = String::new();
-
-    // println!("Insert your new pst data: ");
-    // println!("Title: ");
-    // std::io::stdin().read_line(&mut post_title).unwrap();
-
-    // println!("Body: ");
-    // std::io::stdin().read_line(&mut post_body).unwrap();
-
-    // let splitted: Vec<&str> = post_title.split(" ").collect();
-    // let post_slug = splitted.join("_");
-
-    // let new_post = NewPost { title: post_title.trim(), slug: post_slug.trim(), body: post_body.trim() };
 
     // insert(conn, new_post);
     //
     // let updated_post: Post = diesel::update(posts.filter(id.eq(2))).set(title.eq("Second Updated posts")).get_result(conn).expect("Something wnet wrong in update");
-
 
     // let deleted = diesel::delete(posts.filter(title.like("%second%"))).execute(conn).expect("Error deleting");
 
--- src/models.rs
+++ src/models.rs
@@ -1,10 +1,19 @@
 use diesel::prelude::*;
+use serde::{Deserialize, Serialize};
 
-#[derive(Queryable, Debug)]
+
+#[derive(Queryable, Debug, Deserialize, Serialize)]
 pub struct Post {
     pub id: i32,
     pub title: String,
     pub slug: String,
+    pub body: String,
+}
+
+
+#[derive(Clone, Serialize, Deserialize, Debug)]
+pub struct NewPostHandler {
+    pub title: String,
     pub body: String,
 }
 
@@ -16,4 +25,24 @@ pub struct NewPost<'a> {
     pub title: &'a str,
     pub slug: &'a str,
     pub body: &'a str,
+}
+
+
+impl Post {
+    pub fn define_slug(title: &String) -> String {
+        title.replace(" ", "_").to_lowercase()
+    }
+
+    pub fn create<'a>(conn: &mut PgConnection, post: &NewPostHandler) -> Result<Post, diesel::result::Error> {
+        let slug = Post::define_slug(&post.title.clone());
+
+        let new_post = NewPost {
+            title: &post.title,
+            body: &post.body,
+            slug: &slug,
+        };
+
+        diesel::insert_into(posts::table).values(new_post).get_result::<Post>(conn)
+    }
+
 }
